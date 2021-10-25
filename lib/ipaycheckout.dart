7c682789910fcfb8a8add6086b1dc7bac5d8f7b1
Deskola <denisogunde@gmail.com>
2021-10-25 12:06:25 +0300

    Channel and fields validation

--- lib/ipaycheckout.dart
+++ lib/ipaycheckout.dart
@@ -26,10 +26,26 @@ class IPay {
     required String cbk,
     required String cst,
     required String crl,
+    String mpesa = "1",
+    String bonga = "1",
+    String airtel = "1",
+    String equity = "1",
+    String creditcard = "1",
+    String mobilebanking = "0",
+    String mkoporahisi = "0",
+    String saida = "0",
+    String elipa = "0",
+    String unionpay = "0",
+    String mvisa = "0",
+    String vooma = "0",
+    String pesalink = "0",
+    String autopay = "0",
   }) {
     var key = utf8.encode(vendorSecurityKey);
+    var newPhoneNumber = tel.replaceAll(RegExp(r'[^\w]+'), '');
+    var finalAmount = double.parse(ttl).toStringAsFixed(0);
     var dataString =
-        "$live$oid$inv$ttl$tel$eml$vendorId$curr$p1$p2$p3$p4$cbk$cst$crl";
+        "$live$oid$inv$finalAmount$newPhoneNumber$eml$vendorId$curr$p1$p2$p3$p4$cbk$cst$crl";
     var bytes = utf8.encode(dataString);
 
     var hmacSha1 = Hmac(sha1, key); // HMAC-SHA256
@@ -37,7 +53,7 @@ class IPay {
 
     //Craft url
     final url = baseUrl +
-        "live=$live&oid=$oid&inv=$inv&ttl=$ttl&tel=$tel&eml=$eml&vid=$vendorId&curr=$curr&p1=$p1&p2=$p2&p3=$p3&p4=$p4&cbk=$cbk&cst=$cst&crl=$crl&hsh=$digest&autopay=0";
+        "live=$live&oid=$oid&inv=$inv&ttl=$finalAmount&tel=$newPhoneNumber&eml=$eml&vid=$vendorId&curr=$curr&p1=$p1&p2=$p2&p3=$p3&p4=$p4&cbk=$cbk&cst=$cst&crl=$crl&hsh=$digest&mpesa=$mpesa&bonga=$bonga&airtel=$airtel&equity=$equity&creditcard=$creditcard&mobilebanking=$mobilebanking&mkoporahisi=$mkoporahisi&saida=$saida&elipa=$elipa&unionpay=$unionpay&mvisa=$mvisa&vooma=$vooma&pesalink=$pesalink&autopay=$autopay";
 
     return url;
   }
